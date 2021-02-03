46aff98b1a2886e8f8f5781077f4b5adc17012ed
cdevjarrell <christian.j.jarrell@gmail.com>
2021-02-02 19:28:14 -0500

    Initial commit. Create files for game, gestures, player, and json file.

---
+++ game.js
@@ -0,0 +1,22 @@
+"use strict";
+
+//Properly incorporate inheritance into my game
+//Account for and handle bad user input, ensuring that any user input is validated and reobtained if necessary (validation)
+//Store all of the gestture options/choices in an array. Find a way to utilize array of gestures within code (display options, assign a player a gesture, etc.)
+//Correct player to win a given round based on the choices made by each player
+//RPSLS to be a minimum of "best of three" to decide a winner
+//Option of a single player (human vs AI) or a multipplayer (human vs human) game
+
+
+
+
+//Rock crushes Scissors
+//Scissors cuts Paper
+//Paper covers Rock
+//Rock curshes lizard
+//Lizard poisons Spock
+//Spock smashes Scissors
+//Scissors decapitates Lizard
+//Lizard eats Paper
+//Paper disproves Spock
+//Spock vaporizes Rock
